2f72255af1f91f7d43a1d92a8c745d0863a830c7
vitormcruz <vitormcruz@gmail.com>
2020-04-14 18:08:39 -0300

    Part 35. Colocando os botões.

---
+++ calculadora/src/componentes/Botao.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import "./Botao.css";
+
+export default props =>  <button className='button'>{props.label}</button>
--- calculadora/src/main/Calculadora.jsx
+++ calculadora/src/main/Calculadora.jsx
@@ -1,12 +1,29 @@
 import React, {Component} from "react";
 import "./Calculadora.css"
+import Botao from "../componentes/Botao";
 
 export default class Calculator extends Component {
 
     render() {
         return (
             <div className="calculadora">
-
+                <Botao label="AC"/>
+                <Botao label="/"/>
+                <Botao label="7"/>
+                <Botao label="8"/>
+                <Botao label="9"/>
+                <Botao label="*"/>
+                <Botao label="4"/>
+                <Botao label="5"/>
+                <Botao label="6"/>
+                <Botao label="-"/>
+                <Botao label="1"/>
+                <Botao label="2"/>
+                <Botao label="3"/>
+                <Botao label="+"/>
+                <Botao label="0"/>
+                <Botao label="."/>
+                <Botao label="="/>
             </div>
         )
     }
