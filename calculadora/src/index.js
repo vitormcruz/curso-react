ac17db4404e60c43c6268e34e98c7e189b78e8cd
vitormcruz <vitormcruz@gmail.com>
2020-04-14 17:40:00 -0300

    Part 34. Iniciando a implementação da calculadora.

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Calculadora from "./main/Calculadora";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div>
+        <h1>Calculadora</h1>
+        <Calculadora />
+    </div>,
   document.getElementById('root')
 );
 
---
+++ calculadora/src/main/Calculadora.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import "./Calculadora.css"
+
+export default class Calculator extends Component {
+
+    render() {
+        return (
+            <div className="calculadora">
+
+            </div>
+        )
+    }
+}
