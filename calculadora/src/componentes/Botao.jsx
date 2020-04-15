ba0fed854f0eb909be45e6d90413180e4203f156
vitormcruz <vitormcruz@gmail.com>
2020-04-15 18:09:24 -0300

    Part 37. Ajustando comportamento inicial e colocando cores.

--- calculadora/src/componentes/Botao.jsx
+++ calculadora/src/componentes/Botao.jsx
@@ -1,4 +1,10 @@
 import React from "react";
 import "./Botao.css";
 
-export default props =>  <button className='button'>{props.label}</button>
+export default props =>
+    <button onClick={(() => props.click && props.click(props.label))}
+            className={'button ' + (props.triple ? 'triple ': '') +
+                                   (props.double ? 'double ': '') +
+                                   (props.operation ? 'operation ': '')}>
+        {props.label}
+    </button>
--- calculadora/src/main/Calculadora.jsx
+++ calculadora/src/main/Calculadora.jsx
@@ -5,27 +5,39 @@ import Display from "../componentes/Display";
 
 export default class Calculator extends Component {
 
-    render() {
+    clearMemory = () =>{
+        console.log('limpar')
+    }
+
+    setOperation = (operation) =>{
+        console.log(operation)
+    }
+
+    addDigit = (n) =>{
+        console.log(n)
+    }
+
+    render = () => {
         return (
             <div className="calculadora">
                 <Display value={100}/>
-                <Botao label="AC"/>
-                <Botao label="/"/>
-                <Botao label="7"/>
-                <Botao label="8"/>
-                <Botao label="9"/>
-                <Botao label="*"/>
-                <Botao label="4"/>
-                <Botao label="5"/>
-                <Botao label="6"/>
-                <Botao label="-"/>
-                <Botao label="1"/>
-                <Botao label="2"/>
-                <Botao label="3"/>
-                <Botao label="+"/>
-                <Botao label="0"/>
-                <Botao label="."/>
-                <Botao label="="/>
+                <Botao label="AC" triple click={this.clearMemory}/>
+                <Botao label="/" operation click={this.setOperation}/>
+                <Botao label="7" click={this.addDigit}/>
+                <Botao label="8" click={this.addDigit}/>
+                <Botao label="9" click={this.addDigit}/>
+                <Botao label="*" operation click={this.setOperation}/>
+                <Botao label="4" click={this.addDigit}/>
+                <Botao label="5" click={this.addDigit}/>
+                <Botao label="6" click={this.addDigit}/>
+                <Botao label="-" operation click={this.setOperation}/>
+                <Botao label="1" click={this.addDigit}/>
+                <Botao label="2" click={this.addDigit}/>
+                <Botao label="3" click={this.addDigit}/>
+                <Botao label="+" operation click={this.setOperation}/>
+                <Botao label="0" double click={this.addDigit}/>
+                <Botao label="." click={this.addDigit}/>
+                <Botao label="=" operation click={this.setOperation}/>
             </div>
         )
     }
