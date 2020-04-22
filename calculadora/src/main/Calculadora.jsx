011d84ebb19875adc5a8e3de9e374c83f327703e
vitormcruz <vitormcruz@gmail.com>
2020-04-22 18:33:48 -0300

    Part 39. Fim da implementação da calculadora.

--- calculadora/src/main/Calculadora.jsx
+++ calculadora/src/main/Calculadora.jsx
@@ -3,24 +3,62 @@ import "./Calculadora.css"
 import Botao from "../componentes/Botao";
 import Display from "../componentes/Display";
 
+const initialState = {
+    displayValue: '0',
+    operation: null,
+    values: [null, null],
+    current: 0,
+    limpa: false
+}
+
 export default class Calculator extends Component {
 
+    state = {...initialState};
+
     clearMemory = () =>{
-        console.log('limpar')
+        this.setState({...initialState});
     }
 
     setOperation = (operation) =>{
-        console.log(operation)
+        let values = this.executaOperation(operation, [...this.state.values]);
+        let current = 1;
+        let displayValue = this.state.displayValue;
+        if (operation === '='){
+            operation = null;
+            displayValue = values[0];
+            current = 0;
+        }
+
+        this.setState({current, operation, displayValue, values, limpa: true});
     }
 
-    addDigit = (n) =>{
-        console.log(n)
+    executaOperation = (newOperation, values) => {
+        if(values[1] === null) return values;
+        values[0] =  eval(`${values[0]} ${this.state.operation} ${values[1]}`);
+        if (newOperation === '='){
+            values[1] = null;
+        }
+
+        return values;
+    }
+
+    addDigit = (n) => {
+        if( n === '.' && this.state.displayValue.includes('.')) return;
+
+        let displayValue = this.state.displayValue === '0' ||this.state.limpa  ?
+                            '' : this.state.displayValue;
+        displayValue = displayValue + n;
+
+        let values = [...this.state.values];
+        values[this.state.current] = Number(displayValue);
+        console.log(values);
+        this.setState({ displayValue, values, limpa: false });
     }
 
     render = () => {
         return (
             <div className="calculadora">
-                <Display value={100}/>
+                <Display value={this.state.displayValue}/>
                 <Botao label="AC" triple click={this.clearMemory}/>
                 <Botao label="/" operation click={this.setOperation}/>
                 <Botao label="7" click={this.addDigit}/>
